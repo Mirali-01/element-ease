fb924d6a7d10f58e2c61b84de6940711992cbf60
Mir <mir.m.ali01@gmail.com>
2023-03-22 19:28:41 -0400

    this is getting difficult, i need a break

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
 const CategoryButton = () => {
+  const groupColor = {
+    color: "black",
+    "border-color": "transparent",
+    "background-color": "white",
+  };
+
   return (
-    <div className="sameCategory">
+    <div
+      className="sameCategory"
+      onclick={() => {
+        groupColor();
+      }}
+    >
       <div className="colorBox"></div>
       <h2>Diatomic Nonmetal</h2>
     </div>
