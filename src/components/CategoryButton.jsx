23a89c89d5013b494111646cdc673a51f3623562
Mir <mir.m.ali01@gmail.com>
2023-03-22 21:05:06 -0400

    worked on category button functionality

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -1,23 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 const CategoryButton = () => {
-  const groupColor = {
-    color: "black",
-    "border-color": "transparent",
-    "background-color": "white",
+  const [button, setButton] = useState(0);
+  // use filter method to get features from hover
+  const groupColor = () => {
+    //   <div className="sameCategory">
+    //   <div className="colorBox"></div>
+    //   <h2>Diatomic Nonmetal</h2>
+    // </div>
   };
 
-  return (
-    <div
-      className="sameCategory"
-      onclick={() => {
-        groupColor();
-      }}
-    >
-      <div className="colorBox"></div>
-      <h2>Diatomic Nonmetal</h2>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default CategoryButton;
