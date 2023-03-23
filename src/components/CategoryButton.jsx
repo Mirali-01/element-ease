4f022957e1319ae8451cc90d7f50ee2f3daa88bc
Mir <mir.m.ali01@gmail.com>
2023-03-22 22:42:52 -0400

    tried making the button work, i failed

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { useState } from "react";
+import colorCategory from "../models/ColorCategory";
 
 const CategoryButton = () => {
-  const [button, setButton] = useState(0);
+  const [button, setButton] = useState({});
   // use filter method to get features from hover
-  const groupColor = () => {
-    //   <div className="sameCategory">
-    //   <div className="colorBox"></div>
-    //   <h2>Diatomic Nonmetal</h2>
-    // </div>
-  };
+  // const groupColor = colorCategory.map((color) => {
+  //   return (
+  //     <div className="sameCategory">
+  //       <div className="colorBox"></div>
+  //       <h2>Diatomic Nonmetal</h2>
+  //     </div>
+  //   );
+  // });
 
   return <div></div>;
 };
