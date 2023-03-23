25617f881bfc4ce8c9ba1c84a09781d08e8f87e1
Mir <mir.m.ali01@gmail.com>
2023-03-23 14:34:46 -0400

    dang buttons arent working

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -8,12 +8,15 @@ const CategoryButton = () => {
   const keys = Object.keys(colorCategory[0]); //can map to get all the keys
   const values = Object.values(colorCategory[0]); //can map to get all the values
 
-  const groupColor = [keys, values].map((string) => {
-    console.log(string[0]);
+  const groupColor = [keys, values].map((string, index, arr) => {
+    console.log(string);
+
+    // console.log(arr[0]);
+    // console.log(arr[1]);
     return (
       <div className="sameCategory">
         {/* <div className="colorBox" style={{ backgroundColor: value }}></div> */}
-        <h2>{string[0]}</h2>
+        {/* <h2>{arr[0][count]}</h2> */}
       </div>
     );
   });
