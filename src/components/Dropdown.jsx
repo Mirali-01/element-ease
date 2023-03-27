4334cd394e13add5c39da779779f8e15f614708d
Mir <mir.m.ali01@gmail.com>
2023-03-27 00:32:18 -0400

    cleaning

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 // Future dropdown menu to display more info or highlight certain elements
+// create another component for inputting elements on a search, controlled form handling and submits
 const Dropdown = () => {
   return <div>Dropdown</div>;
 };
