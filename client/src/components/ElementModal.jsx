9bc1905d46de59f7a643e948f63aebd65d4c2424
Mir <mir.m.ali01@gmail.com>
2024-02-23 17:41:13 -0500

    fix: https not http for all server side calls

--- client/src/components/ElementModal.jsx
+++ client/src/components/ElementModal.jsx
@@ -20,7 +20,7 @@ const ElementModal = () => {
   const fetchElement = async (number) => {
     try {
       const response = await axios.get(
-        `http://elementease.onrender.com/element/${number}`
+        `https://elementease.onrender.com/element/${number}`
       );
       // const response = await axios.get(
       //   `http://localhost:5000/element/${number}`
